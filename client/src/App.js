741d099da22cce6636b91e0fa40f6b6b4317fc5d
Cesar Zulueta <cesarzulueta12@gmail.com>
2021-09-21 06:59:29 -0700

    started backend with server models and routes, also started frontend with client folder and react router dom as well

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <input 
+      placeholer="Title" 
+      name="" 
+      value="" 
+      onSubmit="">
+        Title
+      </input>
+      <input 
+      placeholer="Title" 
+      name="" 
+      value="" 
+      onSubmit="">
+        Genre
+      </input>
+    </div>
+  );
+}
+
+export default App;
